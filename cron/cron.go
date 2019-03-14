da84c33ab32ea5e79ca476066e3036119673aebe
mewa <marcin.k.chmiel@gmail.com>
2019-03-14 14:15:01 +0100

    cron: add initial implementation

--- cron/cron.go
+++ cron/cron.go
@@ -1,19 +1,50 @@
 package cron
 
-type Dcron struct{}
+import (
+	c "github.com/mewa/cron"
+)
 
-func New() *Dcron {
-	return &Dcron{}
+type cron struct {
+	cron     *c.Cron
+	entries  map[EntryID]*Entry
+	entryIds map[EntryID]c.EntryID
 }
 
-func (c *Dcron) PutEntry(entry Entry) {}
-
-func (c *Dcron) DeleteEntry(id EntryID) {}
-
-func (c *Dcron) Entry(id EntryID) Entry {
-	return Entry{}
+func New() *cron {
+	return &cron{
+		cron:     c.New(),
+		entries:  map[EntryID]*Entry{},
+		entryIds: map[EntryID]c.EntryID{},
+	}
 }
 
-func (c *Dcron) Start() {}
+func (c *cron) PutEntry(entry Entry) {
+	id := c.cron.Schedule(entry.Schedule, entry.Job)
 
-func (c *Dcron) Stop() {}
+	c.entries[entry.ID] = &entry
+	c.entryIds[entry.ID] = id
+}
+
+func (c *cron) DeleteEntry(id EntryID) {
+	cid, ok := c.entryIds[id]
+	if ok {
+		c.cron.Remove(cid)
+		delete(c.entryIds, id)
+		delete(c.entries, id)
+	}
+}
+
+func (c *cron) Entry(id EntryID) *Entry {
+	if entry := c.entries[id]; entry != nil {
+		return entry
+	}
+	return nil
+}
+
+func (c *cron) Start() {
+	c.cron.Start()
+}
+
+func (c *cron) Stop() {
+	c.cron.Stop()
+}
