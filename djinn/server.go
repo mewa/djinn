c01317b58203e46bfb96c5cddf0801a8c0468ead
mewa <marcin.k.chmiel@gmail.com>
2019-04-13 02:39:59 +0200

    djinn/server: add cron job put http handler

--- djinn/server.go
+++ djinn/server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/mewa/djinn/djinn/job"
 	"io"
 	"net"
 	"net/http"
@@ -13,6 +14,55 @@ type StatusResponse struct {
 	Running bool   `json:"running"`
 	Name    string `json:"name"`
 	Host    string `json:"host"`
+}
+
+type PutCronJobRequest struct {
+	Expression string `json:"schedule"`
+}
+
+type PutCronJobResponse struct {
+	Next int64 `json:"next_execution"`
+}
+
+func (d *Djinn) cronHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	jobId := vars["job"]
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r.Body)
+
+	var s PutCronJobRequest
+	json.Unmarshal(buf.Bytes(), &s)
+
+	// validate input
+	descr := schedule.JSONSchedule{schedule.TypeSpec, s.Expression}
+
+	if _, err := descr.Schedule(); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	resp, err := d.Put(&JobPutRequest{
+		Job: job.Job{
+			ID:         job.ID(jobId),
+			Descriptor: descr,
+		},
+	})
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	httpResp, err := json.Marshal(&PutCronJobResponse{resp.Next})
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Write(httpResp)
 }
 
 func (d *Djinn) statusHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,6 +80,8 @@ func (d *Djinn) statusHandler(w http.ResponseWriter, r *http.Request) {
 func (d *Djinn) Serve() error {
 	r := mux.NewRouter()
 	r.HandleFunc("/status", d.statusHandler)
+	r.HandleFunc("/{job}/cron", d.cronHandler).
+		Methods("PUT")
 
 	d.server = &http.Server{
 		Handler: r,
